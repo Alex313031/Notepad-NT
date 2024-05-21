941fcd71658512b7673522987f9b0cac825b3bbf
Alexander Frick <Alex313031@gmail.com>
2024-05-21 16:02:54 -0500

    use local help

--- notepad/notepad.c
+++ notepad/notepad.c
@@ -799,7 +799,7 @@ INT NPCommand(
                 // NT5 style - on XP and 2k, the required CHM file is located at %WINDIR%\Help\notepad.chm
                 //HtmlHelpA(GetDesktopWindow(), "notepad.chm", HH_DISPLAY_TOPIC, 0L);
                 TCHAR szNpHelpPath[MAX_PATH];
-                ExpandEnvironmentStrings(TEXT("%WINDIR%\\Help\\notepad-nt.chm"), szNpHelpPath, ARRAYSIZE(szNpHelpPath));
+                ExpandEnvironmentStrings(TEXT("notepad-nt.chm"), szNpHelpPath, ARRAYSIZE(szNpHelpPath));
                 ShellExecute(GetDesktopWindow(), NULL, szNpHelpPath, NULL, NULL, TRUE);
             }
             break;
